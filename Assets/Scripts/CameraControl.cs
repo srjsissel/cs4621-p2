032eba3457ac0c8e1ed459e1f862b36e4fd675df
Sissel <rs923@cornell.edu>
2022-09-25 19:10:43 -0400

    Added skybox texture
    Co-authored-by: LiPGa <ll587@cornell.edu>
    Co-authored-by: Claire4923 <zhouxiaoheclaire@163.com>

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    CharacterController controller;
     Vector2 rotation = Vector2.zero;
 	public float speed = 3;
+
+    void Start(){
+        controller = gameObject.GetComponent<CharacterController>();
+    }
 
 	void Update () 
     {
 		rotation.y += Input.GetAxis("Mouse X");
 		rotation.x += -Input.GetAxis("Mouse Y");
-		transform.eulerAngles = (Vector2) rotation * speed;
+		controller.transform.eulerAngles = (Vector2) rotation * speed;
         if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-            transform.Translate(new Vector3(speed * 100 * Time.deltaTime, 0, 0));
+            controller.Move(transform.TransformDirection(new Vector3(speed * 100 * Time.deltaTime, 0, 0)));
         if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-            transform.Translate(new Vector3(-speed * 100 * Time.deltaTime, 0, 0));
+            controller.Move(transform.TransformDirection(new Vector3(-speed * 100 * Time.deltaTime, 0, 0)));
         if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
-            transform.Translate(new Vector3(0, 0, -speed * 100 * Time.deltaTime));
+            controller.Move(transform.TransformDirection(new Vector3(0, 0, -speed * 100 * Time.deltaTime)));
         if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-            transform.Translate(new Vector3(0, 0, speed * 100 * Time.deltaTime));
+            controller.Move(transform.TransformDirection(new Vector3(0, 0, speed * 100 * Time.deltaTime)));
     }
 }
