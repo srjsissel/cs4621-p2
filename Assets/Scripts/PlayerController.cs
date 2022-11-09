9cb71db5b168ec936d940ad6a90d99ce58cf8b4d
Sissel <rs923@cornell.edu>
2022-11-08 19:17:28 -0500

    Added camera and cursor lock

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,51 +8,65 @@ public class PlayerController : MonoBehaviour
     Vector2 rotation = Vector2.zero;
 
     bool isFlying = false;
-	public float speed = 3f;
+    public float speed = 3f;
     public float gravity = 100.0f;
 
     public Camera cam;
     GameObject player;
 
-    void Start(){
+    public GameObject thirdPersonCam;
+    public GameObject flyCam;
+
+    void Start()
+    {
+        thirdPersonCam.SetActive(true);
+        flyCam.SetActive(false);
         controller = gameObject.GetComponent<CharacterController>();
         player = this.gameObject;
     }
 
-	void Update () 
+    void Update()
     {
-        if (!Input.GetKey(KeyCode.LeftControl)){
-            // rotation.y += Input.GetAxis("Mouse X");
-            // rotation.y = cam.transform.rotation.y * 10;
-		    // rotation.x += -Input.GetAxis("Mouse Y");
-        }
-		// controller.transform.eulerAngles = (Vector2) rotation * speed;
-        if(Input.GetKeyDown(KeyCode.Space)){
-            isFlying = !isFlying;
-            if (isFlying) {
-                controller.Move(new Vector3(0, 5f, 0));
+        if (Input.GetKey(KeyCode.LeftControl)){
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            thirdPersonCam.SetActive(false);
+            flyCam.SetActive(false);
+        }else{
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            if (Input.GetKeyDown(KeyCode.Space)){
+                isFlying = !isFlying;
+                if (isFlying){
+                    controller.Move(new Vector3(0, 5f, 0));
+                }
             }
+            thirdPersonCam.SetActive(!isFlying);
+            flyCam.SetActive(isFlying);
+            if (isFlying)
+                flyControl();
+            else
+                walkControl();
         }
 
-        if (isFlying)
-            flyControl();
-        else
-            walkControl();
     }
 
-    void flyControl(){
+    void flyControl()
+    {
 
         float hInput = Input.GetAxis("Horizontal");
         float vInput = Input.GetAxis("Vertical");
 
         Vector3 dir = cam.transform.forward * vInput + cam.transform.right * hInput;
         player.transform.forward = Vector3.Slerp(player.transform.forward, dir, Time.deltaTime * 20);
-        if(dir != Vector3.zero){
+        if (dir != Vector3.zero)
+        {
             controller.Move(player.transform.forward.normalized * speed * 20 * Time.deltaTime);
         }
     }
 
-    void walkControl(){
+    void walkControl()
+    {
         controller.Move(new Vector3(0, -gravity * Time.deltaTime, 0));
 
         float hInput = Input.GetAxis("Horizontal");
@@ -60,7 +74,8 @@ public class PlayerController : MonoBehaviour
 
         Vector3 dir = cam.transform.forward * vInput + cam.transform.right * hInput;
         player.transform.forward = Vector3.Slerp(player.transform.forward, dir, Time.deltaTime * 20);
-        if(dir != Vector3.zero){
+        if (dir != Vector3.zero)
+        {
             player.transform.eulerAngles = Vector3.Scale(player.transform.eulerAngles, new Vector3(0, 1, 1));
             controller.Move(player.transform.forward.normalized * speed * 10 * Time.deltaTime);
         }
