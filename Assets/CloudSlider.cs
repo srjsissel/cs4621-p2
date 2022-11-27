5eddd15277d3e1b3c1a59f3971c5f9af9e4ee2ca
Sissel Sun <rs923@cornell.edu>
2022-11-26 19:34:50 -0500

    Modified cloud slider

--- Assets/CloudSlider.cs
+++ Assets/CloudSlider.cs
@@ -2,25 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloudSlider : MonoBehaviour
+public class CloudSlider: MonoBehaviour
 {
     public static Material cloudMat;
-    public float maxVal = 1f;
+    public static float maxVal = 1.5f;
+    static bool rain = true;
+    static float sliderVal = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
         cloudMat = GetComponent<Renderer>().material;
     }
 
-    public void cloudSlider(float slider){
-        cloudMat.SetFloat("_CloudPower", maxVal / slider);
+    public static void cloudSlider(float slider){
+        sliderVal = slider;
+        if (rain)
+            cloudMat.SetFloat("_CloudPower", (maxVal) / (slider + 0.7f));
+        else 
+            cloudMat.SetFloat("_CloudPower", (maxVal + 0.3f) / (slider + 0.7f));
     }
 
     public static void addRain(bool isRain){
-        if (isRain){
-            cloudMat.SetFloat("_CloudPower", cloudMat.GetFloat("_CloudPower") - 0.5f);
-        } else {
-            cloudMat.SetFloat("_CloudPower", cloudMat.GetFloat("_CloudPower") + 0.5f);
-        }
+        rain = isRain;
+        cloudSlider(sliderVal);
     }
 }
